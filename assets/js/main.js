3afce26fcbac6d6b0a87fe4744393af99a49a62a
mervydevmedia <65782957+kgofweb@users.noreply.github.com>
2022-06-17 00:29:15 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,48 @@ const cardOne = document.querySelector('.card-one')
 const cardTwo = document.querySelector('.card-two')
 const cardThree = document.querySelector('.card-three')
 
-const send = document.getElementById('send'),
-      receive = document.getElementById('receive'),
-      help = document.getElementById('help');
+const send = document.getElementById('send')
+const receive = document.getElementById('receive')
+const help = document.getElementById('help')
 
-
-// Redirect to tranfert page
 send.addEventListener('click', () => {
-  
+  location = 'transfert.html'
+})
+receive.addEventListener('click', () => {
+  showModalCancel('La plateforme est en cours de dévéloppement. Vueillez réesayer plus tard !')
+  // location = 'receive.html'
+})
+help.addEventListener('click', () => {
+  showModalCancel('La plateforme est en cours de dévéloppement. Vueillez réesayer plus tard !')
+  // location = 'help.html'
 })
 
-// Redirect to receive page
-receive.addEventListener('click', () => {
-  
-})
+// Set Error
+let modalWrap = null;
+const showModalCancel = (message) => {
+  if (modalWrap !== null) {
+    modalWrap.remove();
+  }
+  modalWrap = document.createElement('div');
+  modalWrap.innerHTML = `
+    <div class="modal  fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body">
+            <h6>${message}</h6>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-primary" data-bs-dismiss="modal">OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  document.body.append(modalWrap);
+  const modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  modal.show();
+}
 
 const sr = ScrollReveal({
   distance: '70px',
@@ -29,4 +57,4 @@ sr.reveal(title, { origin: 'top', delay: 300 })
 sr.reveal(subTitle, { origin: 'bottom', delay: 400 })
 sr.reveal(cardOne, { origin: 'left', delay: 600 })
 sr.reveal(cardTwo, { origin: 'bottom', delay: 600 })
-sr.reveal(cardThree, { origin: 'right', delay: 600 })
+sr.reveal(cardThree, { origin: 'top', delay: 600 })
--- assets/js/transfert.js
+++ assets/js/transfert.js
@@ -1,1 +1,45 @@
-const cancel = document.getElementById('cancel')
+// DOM element
+const send = document.getElementById('send')
+const load = document.getElementById('load')
+const cancel = document.querySelectorAll('#cancel')
+
+// Loading animation
+onload = () => {
+  setTimeout(() => {
+    load.style.display = 'none'
+  }, 4000)
+}
+
+// Remove all and Return to the home page
+cancel.forEach(el => el.addEventListener('click', () => {
+  showModalCancel('Voulez vous vraiment annuler et revenir à la page d\'accueil ?')
+}))
+
+
+let modalWrap = null;
+const showModalCancel = (message) => {
+  if (modalWrap !== null) {
+    modalWrap.remove();
+  }
+
+  modalWrap = document.createElement('div');
+  modalWrap.innerHTML = `
+    <div class="modal  fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body">
+            <h6>${message}</h6>
+          </div>
+          <div class="modal-footer">
+            <a href='index.html' class="btn btn-primary restor" id='yes'>Oui</a>
+            <button class="btn btn-danger" data-bs-dismiss="modal">Non</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  document.body.append(modalWrap);
+  const modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  modal.show();
+}
